bb42a0690fadc72b4f6a2e933eede4b6294c36d5
Jay035 <jaydencolins08@gmail.com>
2023-06-09 20:45:52 +0100

    fix

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,12 +5,12 @@ export default function NotFound() {
     <div className="text-center px-6 md:px-12 h-screen flex flex-col justify-center items-center gap-4">
       <p className="text-6xl capitalize mb-8 font-medium flex items-center justify-center gap-4 md:text-8xl">
         4{" "}
-        <span className="w-14 h-14 md:w-24 md:h-24 inline-block border border-white rounded-full shadow-2xl shadow-[#EF5B44]"></span>{" "}
+        <span className="w-14 h-14 md:w-24 md:h-24 inline-block border border-white rounded-full shadow-2xl shadow-[#6EE755]"></span>{" "}
         4
       </p>
       <p className="text-base md:text-xl">
         This page doesn't exist.{" "}
-        <Link to="/" className="text-[#EF5B44] tracking-wide">
+        <Link to="/" className="text-[#6EE755] tracking-wide">
           GO HOME
         </Link>
       </p>
