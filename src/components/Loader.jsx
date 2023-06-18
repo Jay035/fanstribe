17a0f8d7c5fad133ddca77e5c864d48f5abf1fc9
Jay035 <jaydencolins08@gmail.com>
2023-06-18 18:40:41 +0100

    update

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,13 @@
+import navlogo from "../assets/logo.png";
+
 export default function Loader() {
   return (
     <div
       id="loader"
       className="animate-pulse flex justify-center items-center min-h-screen bg-[#5eb84c94]"
     >
-      <h1 className="text-3xl md:text-5xl">Fanstribe</h1>
+      {/* <h1 className="text-3xl md:text-5xl">Fanstribe</h1> */}
+      <img src={navlogo} alt="logo" loading="lazy" className="w-12" />
     </div>
   );
 }
