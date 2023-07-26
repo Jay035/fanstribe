e543a35959401cda18195b86ccae947318784f0e
Chibuike Ukandu <codexjay08@gmail.com>
2023-07-27 00:21:44 +0100

    update2

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -17,10 +17,10 @@ export default function Events() {
         </h2>
 
         {events.length > 0 ? (
-          <div className="grid md:grid-cols-2 lg:grid-cols-3">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
             {events?.map((event, index) => (
               <a key={index} href={event?.link} className="" target="_blank" >
-                <img className="hover:opacity-70" src={event.imgUrl} loading="lazy" alt="event image" />
+                <img className="hover:opacity-70 h-fit max-h-[304px] w-full object-cover object-center" src={event.imgUrl} loading="lazy" alt="event image" />
               </a>
             ))}
           </div>
